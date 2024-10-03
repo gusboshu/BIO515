827d8b5de215a83c623085155fb39290c104a58f
gusboshu <gusboshu@student.gu.se>
2024-10-03 14:54:21 +0200

    fixed

---
+++ Bio515.R
@@ -0,0 +1,17 @@
+BIO515
+
+
+3 oct
+
+
+rm(list = ls())
+
+
+## install if needed (do this exactly once):
+install.packages("usethis")
+
+library(usethis)
+use_git_config(user.name = "gusboshu", user.email = "gusboshu@student.gu.se")
+
+
+
